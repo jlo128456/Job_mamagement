29338fa793b22e7840b9818c6aed2dc934a75bda
Jeffrey <j.lo128456@gmail.com>
2025-06-07 13:05:33 +1000

    update seed.py

--- backend/seeders/user_seeder.py
+++ backend/seeders/user_seeder.py
@@ -1,15 +1,15 @@
 from models.models import db, User
 from werkzeug.security import generate_password_hash
 
-
 def seed_users():
     users = [
-        User(id=1001, email="admin1@example.com", password=..., role="admin"),
-        User(id=2001, email="jeffl@example.com", password=..., role="contractor"),
-        User(id=2002, email="contractor2@example.com", password=..., role="contractor"),
-        User(id=2003, email="contractor3@example.com", password=..., role="contractor"),
-        User(id=6135, email="darren@example.com", password=..., role="technician"),
+        User(id=1001, email="admin1@example.com", password=generate_password_hash("admin123"), role="admin"),
+        User(id=2001, email="jeffl@example.com", password=generate_password_hash("contractor123"), role="contractor", contractor="JeffL"),
+        User(id=2002, email="contractor2@example.com", password=generate_password_hash("contractor123"), role="contractor"),
+        User(id=2003, email="contractor3@example.com", password=generate_password_hash("contractor123"), role="contractor"),
+        User(id=6135, email="darren@example.com", password=generate_password_hash("tech123"), role="technician", contractor="Darren"),
     ]
+
     db.session.add_all(users)
     db.session.flush()
 
